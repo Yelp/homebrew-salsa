5d5e63dd069b699701d48a8ba63106c49ccb66cc
Max Rabiciuc <maxr@yelp.com>
2018-04-13 13:57:28 -0700

    initial commit

---
+++ salsa.rb
@@ -0,0 +1,18 @@
+class Salsa < Formula
+  desc "A tool for creating sketch files using the iOS simulator"
+  homepage "https://github.com/Yelp/salsa"
+  url "https://github.com/Yelp/salsa.git",
+    :using => :git,
+    :tag => 'v0.6.0'
+  version "0.6.0"
+  depends_on :xcode => "9.3"
+  
+  def install
+    system "make", "binary"
+    bin.install "SalsaCompiler/build/Release/salsa"
+  end
+
+  test do
+    system bin/"salsa", "--help"
+  end
+end
